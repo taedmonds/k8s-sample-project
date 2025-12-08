43ab98845aed68897973511b628f374638b29437
Terance Edmonds <teranceedmonds20@gmail.com>
2025-12-08 09:40:29 +0530

    kafka configuration update & helm chart configuration fixes

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,6 +1,13 @@
 from kafka import KafkaProducer
 import json
 
-producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9094",
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+    api_version=(3, 8, 0),
+    retries=3,
+    request_timeout_ms=30000
+)
 producer.send('test-topic', {'message': 'Hello, Kafka from Python!'})
 producer.flush()
+print("Message sent!")
